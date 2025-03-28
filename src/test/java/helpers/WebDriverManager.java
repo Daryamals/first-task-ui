fe991858a176ef4536230db35770275dd9bb5c52
omanytea <omanytea@school21.ru>
2025-03-28 17:01:53 +0300

    setting CI/CD

--- src/test/java/helpers/WebDriverManager.java
+++ src/test/java/helpers/WebDriverManager.java
@@ -4,10 +4,17 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriverManager {
 	public static WebDriver createDriver() {
-		WebDriver driver = new ChromeDriver();
+//		WebDriver driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless=new");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		WebDriver driver = new ChromeDriver(options);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		return driver;
 	}
