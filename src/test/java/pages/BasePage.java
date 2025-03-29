11a6e563d8f4bc8219fd238c2ca5c5a3581888d3
omanytea <omanytea@school21.ru>
2025-03-29 22:22:33 +0300

    edit BasePage

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,27 +1,39 @@
 package pages;
 
+import helpers.WaitHelper;
 import io.qameta.allure.Step;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class ManagerPage extends BasePage {
-    private By addCustomerButton = By.xpath("//button[contains(text(), 'Add Customer')]");
+public abstract class BasePage {
 
-    private By customersButton = By.xpath("//button[contains(text(), 'Customers')]");
+    protected WebDriver driver;
 
-    public ManagerPage(WebDriver driver) {
-        super(driver);
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+    @Step("Кликнуть на элемент {locator}")
+    protected void click(By locator) {
+        WebElement element = driver.findElement(locator);
+        WaitHelper.waitForElementToBeClickable(driver, element, 10);
+        element.click();
+    }
+    @Step("Ввести {text} в элемент {locator}")
+    protected void sendKeys(By locator, String text) {
+        WebElement element = driver.findElement(locator);
+        WaitHelper.waitForElementVisibility(driver, element, 10);
+        element.clear();
+        element.sendKeys(text);
+    }
+    @Step("Получить аллерт")
+    public String getAlertText() {
+        WaitHelper.waitForAlert(driver, 10);
+        return driver.switchTo().alert().getText();
     }
 
-    @Step("Переход на страницу добавления клиента")
-
-    public void clickAddCustomer() {
-        click(addCustomerButton);
+    public void acceptAlert() {
+        driver.switchTo().alert().accept();
     }
-
-    @Step("Переход на страницу клиентов")
-
-    public void clickCustomers() {
-        click(customersButton);
-    }
-}
+} 
